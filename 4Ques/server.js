1851d0438e5793dbc190d32074324f054bd5e7e8
DHURANDHAR6969 <gopinathchowdary13@gmail.com>
2026-04-18 07:34:22 +0530

    1to5

--- 4Ques/server.js
+++ 4Ques/server.js
@@ -1,3 +1,30 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "password",
+    database : "node_lab"
+});
+
+db.connect(err =>{
+    if(err){
+        console.log("error");
+    }
+    else{
+        console.log("suXess");
+    }
+
+});
+
+
 app.post("/pay", (req, res) => {
     const { cardno, cvv, exp, otp } = req.body;
 
